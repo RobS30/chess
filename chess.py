68174cc26fe9b389cbd22bab91ceac668cabd5cc
RobS <51037713+RobS30@users.noreply.github.com>
2020-12-13 17:32:46 -0600

    added dev outline

--- chess.py
+++ chess.py
@@ -12,6 +12,7 @@ class Chess:
         self._white_pieces = [w.R1,w.Kn1,w.B1,w.K,w.Q,w.B2,w.Kn2,w.R2,w.p1,w.p2,w.p3,w.p4,w.p5,w.p6,w.p7,w.p8]
         self._black_pieces = [b.R1,b.Kn1,b.B1,b.K,b.Q,b.B2,b.Kn2,b.R2,b.p1,b.p2,b.p3,b.p4,b.p5,b.p6,b.p7,b.p8]
 
+
     
     def print_board(self):
         """used for debugging- prints the board to console alng with key variable values"""
